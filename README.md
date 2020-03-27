396630c4cf7f548fb17415a29c0558eea1aca1a5
Tj Thouhid <tjthouhid@gmail.com>
2020-03-27 11:31:05 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Corona India Live Update
+
+This Plugin get live data from https://www.mohfw.gov.in/ and update to your wordpress Database in every 30min.
+
+You can then use Shortcode to See the live update on your website.
+
+## Here how you can use it : 
+```
+[corona-ilu-info name='confirmed_case'] 
+```
+** This is for showing Active COVID 2019 cases **
+
+You can use dffrent name attribute value for diffrent values.
+Plugin Menu Have All values Given.
+
+## How to Install
+
+
+### From this repository
+
+Go to the [releases](https://github.com/tjthouhid/Corona-India-Live-Update/releases) section of the repository and download the most recent release.
+
+Then, from your WordPress administration panel, go to `Plugins > Add New` and click the `Upload Plugin` button at the top of the page.
---
+++ corona-india-live-update.php
@@ -0,0 +1,225 @@
+<?php
+/* 
+Plugin Name: Corona India Live Update
+Plugin URI: https://github.com/tjthouhid/Corona-India-Live-Update/
+Description: This Plugin get live data from https://www.mohfw.gov.in/ and update. 
+Version: 1.0.2 
+Author: Tj Thouhid 
+Author URI: https://tjthouhid.me/
+License: GPLv2 or later 
+
+    */
+   
+
+
+global $jal_db_version;
+$jal_db_version = '1.0';
+
+function corona_ilu_install() {
+	global $wpdb;
+	global $jal_db_version;
+
+	$table_name = $wpdb->prefix . 'corona_ilu_info';
+	
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		last_data int(100) NOT NULL,
+		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		passengers_screened_at_airport varchar(100) DEFAULT '' NOT NULL,
+		#passengers_screened_at_airport_info text NOT NULL,
+		confirmed_case varchar(100) DEFAULT '' NOT NULL,
+		#confirmed_case_info text NOT NULL,
+		recovered_case varchar(100) DEFAULT '' NOT NULL,
+		#recovered_case_info text NOT NULL,
+		death varchar(100) DEFAULT '' NOT NULL,
+		#death_info text NOT NULL,
+		migrated varchar(100) DEFAULT '' NOT NULL,
+		#migrated_info text NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'jal_db_version', $jal_db_version );
+
+	wp_schedule_event( time(), '30min', 'cli_my_hourly_event' );
+}
+register_activation_hook( __FILE__, 'corona_ilu_install' );
+
+function cli_my_cron_schedules($schedules){
+    if(!isset($schedules["5min"])){
+        $schedules["5min"] = array(
+            'interval' => 5*60,
+            'display' => __('Once every 5 minutes'));
+    }
+    if(!isset($schedules["30min"])){
+        $schedules["30min"] = array(
+            'interval' => 10*60,
+            'display' => __('Once every 30 minutes'));
+    }
+    return $schedules;
+}
+add_filter('cron_schedules','cli_my_cron_schedules');
+
+register_deactivation_hook( __FILE__, 'my_deactivation' ); 
+function my_deactivation() {
+	global $wpdb;
+    $table_name = $wpdb->prefix . 'corona_ilu_info';
+    $sql = "DROP TABLE IF EXISTS $table_name";
+    $wpdb->query($sql);
+    wp_clear_scheduled_hook( 'cli_my_hourly_event' );
+}
+function corona_ilu_install_data() {
+	global $wpdb;
+	
+	$table_name = $wpdb->prefix . 'corona_ilu_info';
+	
+	$wpdb->insert( 
+		$table_name, 
+		array( 
+			'time' => current_time( 'mysql' ), 
+			'last_data' => '101', 
+			'passengers_screened_at_airport' => '0', 
+			//'passengers_screened_at_airport_info' => '0', 
+			'confirmed_case' => '0', 
+			//'confirmed_case_info' => '0', 
+			'recovered_case' => '0', 
+			//'recovered_case_info' => '0', 
+			'death' => '0', 
+			//'death_info' => '0'  
+			'migrated' => '0', 
+			//'migrated_info' => '0' 
+		) 
+	);
+}
+
+register_activation_hook( __FILE__, 'corona_ilu_install_data' );
+
+function update_corona_ilu_info() {
+	global $wpdb;
+	
+	$table_name = $wpdb->prefix . 'corona_ilu_info';
+
+
+	$url="https://www.mohfw.gov.in/";  
+	$original_file = file_get_contents("$url");
+
+	//preg_match_all( "@<h2 class=\"case\">(.*)</h2>@siU", $original_file, $case );
+	//preg_match_all( "@<span class=\"info\">(.*)</span>@siU", $original_file, $case_info );	
+
+	preg_match_all( "@<span class=\"icount\">(.*)</span>@siU", $original_file, $case );
+	preg_match_all( "@<div class=\"info_label\">(.*)</div>@siU", $original_file, $case_info );
+	$data = array(
+		'time' => current_time( 'mysql' ),
+		'passengers_screened_at_airport' => $case[1][0], 
+		//'passengers_screened_at_airport_info' => '0',  
+		'confirmed_case' => $case[1][1], 
+		//'confirmed_case_info' => $case_info[1][0], 
+		'recovered_case' => $case[1][2], 
+		//'recovered_case_info' => $case_info[1][3], 
+		'death' => $case[1][3], 
+		//'death_info' => $case_info[1][4],
+		'migrated' => $case[1][4], 
+		//'migrated_info' => '0',
+	);
+	// echo "<pre>";
+	// print_r($data);
+	// echo "</pre>";
+	$wpdb->update($table_name, $data, array('last_data'=>'101'));
+}
+
+add_action( 'cli_my_hourly_event', 'update_corona_ilu_info' );
+
+
+
+
+
+
+function corona_ilu_info_shortcode( $atts ) {
+   $a = shortcode_atts( array(
+      'name' => 'confirmed_case'
+   ), $atts );
+   global $wpdb;
+   $name = $a['name'];
+   //update_corona_ilu_info();
+$table_name = $wpdb->prefix . 'corona_ilu_info';
+   $post_data = $wpdb->get_results("SELECT $name FROM $table_name WHERE last_data = '101'");
+  // echo "<pre>";
+  // print_r($post_data[0]->$name);
+   return $post_data[0]->$name;
+}
+add_shortcode( 'corona-ilu-info', 'corona_ilu_info_shortcode' );
+
+add_action( 'admin_menu', 'corona_ilu_menu' );
+
+function corona_ilu_menu() {
+	$page_title='Corona India Live Update';
+	$menu_title='Corona ILU';
+	$capability=1;
+	$menu_slug='corona_ilu_menu';
+	$function='corona_ilu_template';
+	$icon_url='';
+	$position=69.9;
+	// $ch_menu_slug="fit_slider_setting";
+	// $ch_page_title="FIT Slider Setting";
+	// $ch_menu_title="Setting";
+	// $ch_function="fit_sl_setting";
+
+	add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+	//add_submenu_page( $menu_slug, $ch_page_title, $ch_menu_title, $capability, $ch_menu_slug, $ch_function );
+	//add_options_page( $page_title, $menu_title, $capability,$menu_slug,$function);
+}
+
+function corona_ilu_template(){
+	?>
+	<style type="text/css">
+		.result-clb{margin-top: 60px;}
+		.clb{font-size: 18px;font-weight: bold;}
+		.clb_spn{font-size: 15px;font-weight: bold;color: #da1324;background-color: #fff;padding: 20px;}
+		.result-clb table{background-color: #fff;width: 400px;}
+		.result-clb table td{padding: 10px;}
+		.attr-rs{color: #da1324;font-weight: bold;}
+	</style>
+	<h1>Corona India Live Update</h1>
+	<div class="result-clb">
+		<label class="clb">Shordcode : </label>
+		<span class="clb_spn"> [corona-ilu-info name='confirmed_case']</span>
+	</div>
+	<div class="result-clb">
+		<label class="clb">Attribute 'name' Values : </label>
+		<br>
+		<br>
+		<table border="1">
+			<tr>
+				<th>RESULT</th>
+				<th>attribute</th>
+			</tr>
+			<tr>
+				<td>Passengers screened at airport</td>
+				<td class="attr-rs">passengers_screened_at_airport</td>
+			</tr>
+			<tr>
+				<td>CONFIRMED CASES</td>
+				<td class="attr-rs">confirmed_case</td>
+			</tr>
+			<tr>
+				<td>RECOVERED CASES</td>
+				<td class="attr-rs">recovered_case</td>
+			</tr>
+			<tr>
+				<td>DEATHS</td>
+				<td class="attr-rs">death</td>
+			</tr>
+			<tr>
+				<td>Migrated</td>
+				<td class="attr-rs">migrated</td>
+			</tr>
+		</table>
+	</div>
+	
+	<?php 
+
+}
